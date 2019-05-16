790bda6534210cc33c868ab22f57c7c2e3187e35
Cristian371 <czend371@gmail.com>
2019-05-16 18:28:04 -0500

    added a funtion to clear search bar when submitted

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var topics = ["Chowder", "Adventure Time", "Regular Show"];
 
-
 // iterates through array and creates buttons for each.
 function renderButtons() {
     $(".button-area").empty();
@@ -16,20 +15,18 @@ function renderButtons() {
 // This function handles events where the add topic button is clicked
 $("#go-button").click(function (event) {
     event.preventDefault();
-
     var input = $("#search-term").val().trim();
 
     topics.push(input);
     renderButtons();
+    $("#search-term").val("");
+
 })
-
 renderButtons();
-
 
 
 // click event that initiates request to API
 $(document.body).on("click", ".press", function () {
-    // $(document.body).on("click", ".checkbox", function()
     var show = $(this).attr("data-show");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=kW9sQ9OUs0tX14I9S6vmi6kXenh8dyx4&limit=10";
 
@@ -43,6 +40,8 @@ $(document.body).on("click", ".press", function () {
         for (var j = 0; j < results.length; j++) {
             rating = $("<div/>").text("Rating: " + results[j].rating);
             $("#gifdiv").append(rating);
+            title = $("<div/>").text("Title: " + results[j].title);
+            $("#gifdiv").prepend(title);
             // gave image attr of still image
             var image = $("<img>");
             image.attr("src", results[j].images.fixed_height_still.url);
